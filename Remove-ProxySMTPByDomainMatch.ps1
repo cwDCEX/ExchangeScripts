5e6af2391ef069837bb13de0fda267d4364cbbed
cwDCEX <126429844+cwDCEX@users.noreply.github.com>
2023-02-27 14:00:04 +1300

    Remove-ProxySMTPByDomainMatch.ps1
    Removes SMTP proxy email addresses from an AD user account if they contain $DomainToRemove. 
    Uses Exchange Management Shell.
    Requires CSV file for inputting AD user accounts. Specify AD user by SAMAccountName with 'AccountName' column header.

---
+++ Remove-ProxySMTPByDomainMatch.ps1
@@ -0,0 +1,44 @@
+# Import the Exchange Management Shell module
+if (-not (Get-Module -Name ExchangeManagementShell -ErrorAction SilentlyContinue)) {
+    Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+}
+
+# Set the path to the CSV file containing the list of users
+$CSVPath = "C:\Temp\Users.csv"
+
+# Set the domain to remove from the email addresses
+$DomainToRemove = "europeanfinance.co.nz"
+
+# Load the list of users from the CSV file
+$Users = Import-Csv -Path $CSVPath
+
+# Loop through each user in the CSV file
+foreach ($User in $Users) {
+    Write-Host "Processing user $($User.AccountName)"
+
+    # Get the mailbox for the user
+    $Mailbox = Get-Mailbox -Identity $User.AccountName -ErrorAction Continue
+
+    if ($Mailbox) {
+        # Get the list of email addresses for the mailbox
+        $EmailAddresses = $Mailbox.EmailAddresses | 
+            Where-Object { $_.PrefixString -ne "smtp" -or $_.SmtpAddress.ToLower() -notlike "*@$DomainToRemove" } |
+            ForEach-Object { [Microsoft.Exchange.Data.ProxyAddress]::Parse($_) }
+
+        # Update the email addresses for the mailbox
+        try {
+            Set-Mailbox -Identity $User.AccountName -EmailAddresses $EmailAddresses -ErrorAction Stop
+        }
+        catch {
+            if ($_.Exception.GetType().FullName -eq "Microsoft.Exchange.Data.ProxyAddressExistsException") {
+                Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
+            }
+            else {
+                throw $_.Exception
+            }
+        }
+    }
+    else {
+        Write-Host "Error: Mailbox not found for $($User.AccountName)" -ForegroundColor Red
+    }
+}
